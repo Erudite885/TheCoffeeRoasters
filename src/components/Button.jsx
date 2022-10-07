43f8a389ecf19063309551080f2daefb9b7888eb
Erudite885 <erudite885@gmail.com>
2022-10-07 10:04:08 +0100

    Button and animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { arrowUp } from '../assets'
 
 const Button = ({styles}) => {
   return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>Get Started</button>
-  )
+    <button
+      type="button"
+      className={`py-4 px-6 border uppercase font-poppins flex flex-row font-medium text-[18px] text-white outline-none ${styles}`}
+    >
+      view our menu
+      <div> 
+        <img src={arrowUp} className="ml-5 animate-bounce" />
+      </div>
+    </button>
+  );
 }
 
 export default Button
