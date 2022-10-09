559f1bb904148711c60053ecd0cf9816f42a30ca
Erudite885 <erudite885@gmail.com>
2022-10-09 02:35:50 +0100

    CTA fixed: gradient, heading, text, &  button

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import styles from "../style";
-import Button from "./Button";
+import { arrowUp } from "../assets";
+// import Button from "./Button";
 
 const CTA = () => (
-  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-gradient-to-r from-amber-900 to-black  rounded-[20px] box-shadow`}
+  >
     <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Let's try our service now!!</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Everything you need to accept card payments and grow your business
-        anywhere on the planet.
+      <h2 className={styles.heading2}>Looking for a fresh cup of coffee?</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 text-center`}>
+        Come drink coffee with us at <br /> Fleet Street, Chancery Lane, or
+        Mikannourd Road.
       </p>
     </div>
 
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-      <Button styles="rounded-[10px]" />
+      <button
+        type="button"
+        className={`py-4 rounded-[8px] animate-bounce px-6 border uppercase font-poppins flex flex-row font-medium text-[18px] text-white outline-none ${styles}`}
+      >
+        Book a Table
+      </button>
     </div>
   </section>
 );
