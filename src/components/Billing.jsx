0df39362de200b2fac951ee753746cb0fb47d652
Erudite885 <erudite885@gmail.com>
2022-10-09 06:02:08 +0100

    Billing: FeaturesCard, Features & Heading added

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,45 +1,56 @@
 import React from 'react'
 
-import { google, bill, apple } from '../assets'
+import { features, featuresFour, featuresThree, featuresTwo } from "../constants";
+import { barista2, beans } from '../assets'
 import styles, { layout } from '../style'
 
-const Billing = () => (
-  <section id="product" className={layout.sectionReverse}>
-    <div className={layout.sectionImgReverse}>
-      <img
-        src={bill}
-        alt="billing"
-        className="w-[100%] h-[100%] relative z-[5]"
-      />
-      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
-      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full bg-dimAmber ${styles.flexCenter}`}
+    >
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] rounded-full object-contain" />
     </div>
-
-    <div className={layout.sectionInfo}>
-      <h2 className={`${styles.heading2} mt-5`}>
-        Easily control your <br className="sm:block hidden" /> billing &
-        invoicing.
-      </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultricies ametodio
-        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+    <div className="flex-1 flex flex-col ml-3 ">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
       </p>
-
-      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img
-          src={apple}
-          alt="apple"
-          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer "
-        />
-        <img
-          src={google}
-          alt="google"
-          className="w-[128px] h-[42px] object-contain cursor-pointer "
-        />
-      </div>
     </div>
-  </section>
+  </div>
+);
+
+const Billing = () => (
+  <>
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-0 relative z-[1]">
+      <h1 className={styles.heading2}>Our Menu</h1>
+    </div>
+
+    <section id="product" className={`${layout.section} bg-stone-800 rounded-[20px] md:rounded-[16px] relative mt-0 mb-10`}>
+      <div className={layout.sectionImg}>
+        <div className={`${layout.sectionImgReverse} flex-col`}>
+          {featuresThree.map((feature, index) => (
+            <FeatureCard key={feature.id} {...feature} index={index} />
+          ))}
+        </div>
+
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full amber__gradient" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {featuresFour.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
+  </>
 );
 
 export default Billing
