008a42aa74b67add0edb3f80d61f73525bca19ce
Erudite885 <erudite885@gmail.com>
2022-10-09 02:28:38 +0100

    Footer fixed: logo, text, & All rights reserved.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () =>
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className='flex-1 flex flex-col justify-start mr-10'>
-        <img src={hotCoffee} alt="hoobank" className='w-[266px] h-[72px] object-contain' />
-        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy reliable and secure.</p>
+        <img src={hotCoffee} alt="The coffee Roasters logo" className='w-[266px] h-[72px] object-contain' />
+        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>Not just a drop-in get coffee service, but a new way to Sit, Relax and enjoy that tailor-made cup of coffee.</p>
       </div>
 
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
@@ -28,7 +28,7 @@ const Footer = () =>
     </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[#3f3e45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">HooBank. All Rights Reserved.</p>
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">The Coffee Roasters. All Rights Reserved.</p>
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, index) => (
           <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`} />
